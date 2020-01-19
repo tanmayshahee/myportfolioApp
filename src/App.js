33e9a4aa227e1186839d9f6ec6227306917a5535
Tanmay Shah <tanmayshahee@gmail.com>
2020-01-19 19:51:49 +0530

    sliding banner

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import SlidingBanner from "./components/SlidingBanner";
 function App() {
-  return <Sidebar />;
+  return (
+    <>
+      <Sidebar />
+      <SlidingBanner />
+    </>
+  );
 }
 
 export default App;
